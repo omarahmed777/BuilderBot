0ee57b4d17baf3a325cb49d983570a97b95e98a5
Omar Ahmed <88595874+omarahmed777@users.noreply.github.com>
2023-01-23 20:01:53 +0000

    Fixed grammar issues

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 
-const token = process.env['token']
-const CLIENT_ID = process.env['client_id']
-const GUILD_ID = process.env['guild_id']
+const token = process.env['token'];
+const CLIENT_ID = process.env['client_id'];
+const GUILD_ID = process.env['guild_id'];
 
-const commands = [{
-  name: 'givechamp',
-  description: 'Replies with a chamion suggestion!'
-}]; 
-console.log(CLIENT_ID)
+const commands = [
+	{
+		name: 'givechamp',
+		description: 'Replies with a champion suggestion!'
+	}
+];
+console.log(CLIENT_ID);
 const rest = new REST({ version: '9' }).setToken(token);
 
 (async () => {
-  try {
-    console.log('Started refreshing application (/) commands.');
+	try {
+		console.log('Started refreshing application (/) commands.');
 
-    await rest.put(
-      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-      { body: commands },
-    );
+		await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+			body: commands
+		});
 
-    console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
-  }
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
 })();
